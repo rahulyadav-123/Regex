39f497690b41e39f08d979f61747218255cc0d76
rahul yadav <ry219613@gmail.com>
2021-10-29 19:52:57 +0530

    added pattern for first name

--- com/Regix/UserRegistration.java
+++ com/Regix/UserRegistration.java
@@ -1,68 +1,24 @@
 package com.Regix;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public  void firstName(String firstName) {
-		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-		Matcher match = pattern.matcher(firstName);
-		if(match.find() && match.group().equals(firstName)) {
-			System.out.println("valid FirstName");	
-			}
-		else {
-			System.out.println("invalid FirstName");
-			}
-        }
-	public void lastName(String lastName) {
-		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-		Matcher match = pattern.matcher(lastName);
-		if(match.find() && match.group().equals(lastName)) {
-			System.out.println("valid LastName");	
-			}
-		else {
-			System.out.println("invalid lastName");
-			}
-        }
-	public void emailId(String emailId) {
-		Pattern pattern = Pattern.compile("^[a-zA-Z0-9+_.-]*[a-zA-Z0-9]?@[a-zA-Z0-9]+.([.][a-zA-Z]+)+$");
-		Matcher match = pattern.matcher(emailId);
-		if(match.find() && match.group().equals(emailId)) {
-			System.out.println("valid Email");	
-			}
-		else {
-			System.out.println("invalid Email");
-			}
-        }
-	public void phoneNumber(String phoneNumber) {
-		Pattern pattern = Pattern.compile("^(\\d{1,2}?)[- .]?\\d{9,10}$");
-		Matcher match = pattern.matcher(phoneNumber);
-		if(match.find() && match.group().equals(phoneNumber)) {
-			System.out.println("valid phoneNumber");	
-			}
-		else {
-			System.out.println("invalid phoneNumber");
-			}
-        }
-			public void password(String password) {
-				Pattern pattern = Pattern.compile("^[a-z](?=.*[A-Z])(?=.*[0-9])(?=.*[\\w\\d]).{8,}$");
-				Matcher match = pattern.matcher(password);
-				if(match.find() && match.group().equals(password)) {
-					System.out.println("valid password");	
-					}
-				else {
-					System.out.println("invalid password");
-					}
-		    }
-				
-	}
-
 	
-	
-		
-	
-	
-		
-	
-
+        public static void main(String[] args) {
+        	Scanner sc = new Scanner(System.in);
+    		System.out.println("Enter the first name: ");
+    		String firstName = sc.next();	
+    		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+    		Matcher match = pattern.matcher(firstName);
+    		if(match.find() && match.group().equals(firstName)) {
+    			System.out.println("valid FirstName");	
+    			}
+    		else {
+    			System.out.println("invalid FirstName");
+    			}
+            }
+    
+}
 
